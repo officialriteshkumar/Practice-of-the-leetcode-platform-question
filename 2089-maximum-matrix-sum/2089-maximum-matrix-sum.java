d5fcf72cb32f2e3e355a250ad4680d68fe977f9f
Ritesh kumar <115397558+officialriteshkumar@users.noreply.github.com>
2024-11-24 22:12:40 +0530

    Time: 6 ms (91.07%), Space: 55.2 MB (34.5%) - LeetHub

---
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int minPos = Integer.MAX_VALUE, maxNeg = Integer.MIN_VALUE, negatives = 0;
+        long sum = 0;
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < 0) {
+                    negatives += 1;
+                    sum -= matrix[i][j]; 
+                    maxNeg = Math.max(maxNeg, matrix[i][j]);
+                } else {
+                    sum += matrix[i][j];
+                    minPos = Math.min(minPos, matrix[i][j]);
+                }
+            }
+        }
+        
+        if ((negatives & 1) == 0) {
+            return sum;
+        }
+        
+        return sum - 2 * Math.min(-maxNeg, minPos);
+    }
+}
